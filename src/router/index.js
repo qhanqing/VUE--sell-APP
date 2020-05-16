6338312e5f9cf31d4ea47a6b918facbfad75cf36
hanqing <851636176@qq.com>
2020-05-16 16:53:03 +0800

    微调

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import goods from '@/components/goods/goods'
 import ratings from '@/components/ratings/ratings'
 import seller from '@/components/seller/seller'
@@ -13,8 +12,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: '商品',
+      component: goods
     },
     {
       path: '/goods',
